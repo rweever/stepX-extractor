394496deda9c7ef234e9afc42780196fb1c827a1
rweever <rweever@finance.gov.gy>
2021-12-02 09:30:26 -0400

    Update environment.py

--- config/environment.py
+++ config/environment.py
@@ -1,8 +1,8 @@
 
-wb_credentials = {"email": '', 'password': ''}
-geckodriver_path = r''
+wb_credentials = {"email": 'email@test.com', 'password': 'password123'}
+geckodriver_path = r'/Users/userName/webdriverLocationFolder/geckodriver'
 database_name = "stepX"
 database_url = "localhost"
 
-PROJECTS_LIST =['']
+PROJECTS_LIST =['PROJECT_ID']
 
